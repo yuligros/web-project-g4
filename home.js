91c9760b41b57d516c4231cd0e1756becf5d7521
yuligros <yuligros11@gmail.com>
2021-12-06 17:45:35 +0200

    yuli commited

--- home.js
+++ home.js
@@ -1,11 +1,11 @@
-var imgs = [
-        "images/asian.PNG",
-        "images/beit_kandinof.PNG",
-        "images/wine.PNG",
-        "images/food.PNG",
-        "images/gaia_garden.png"
+let imgs = [
+    "images/asian.PNG",
+    "images/beit_kandinof.PNG",
+    "images/wine.PNG",
+    "images/food.PNG",
+    "images/gaia_garden.png"
 ];
-var i = 0;
+let i = 0;
 
 function Motion(){
     setTimeout(function(){
@@ -18,4 +18,10 @@ function Motion(){
             i = 0;
         }
     }, 2000);
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
